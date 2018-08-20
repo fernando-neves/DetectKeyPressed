a257d9e13d6034b38a03e7241ea64e9de787b8a1
FernandoNeves <nando1997.neves@gmail.com>
2018-08-20 17:14:43 -0300

    implemented get foregound windows process id

--- DetectKeyPressed/hotkey/hotkey_manager.h
+++ DetectKeyPressed/hotkey/hotkey_manager.h
@@ -11,7 +11,7 @@ public:
 	}
 
 	void initialize(
-		std::function<void(uint32_t, bool, bool)> _on_callback
+		std::function<void(uint32_t, uint32_t, bool, bool)> _on_callback
 	) {
 		static bool _started_thread = false;
 		if (_started_thread)
@@ -23,7 +23,7 @@ public:
 	}
 
 	void set_callback(
-		std::function<void(uint32_t, bool, bool)> _on_callback
+		std::function<void(uint32_t, uint32_t, bool, bool)> _on_callback
 	) {
 		on_callback = _on_callback;
 	}
@@ -37,7 +37,6 @@ public:
 	}
 
 private:
-
 	bool is_ctrl_pressed(
 	) {
 		return 0 != (GetKeyState(VK_CONTROL) & 0x8000);
@@ -48,26 +47,34 @@ private:
 		return 0 != (GetKeyState(VK_SHIFT) & 0x8000);
 	}
 	
+	uint32_t get_process_id(
+	) {
+		HWND hWnd = GetForegroundWindow();
+		DWORD dwProcessId;
+		GetWindowThreadProcessId(hWnd, &dwProcessId);
+		return;
+	}
+
 	DWORD runner_thread(
 	) {
 		for (;;) {
-			for (int i = 0x30; i <= 0x5A; i++) {
-				if (VK_CONTROL == i || VK_SHIFT == i)
+			for (int key_id = 0x30; key_id <= 0x5A; key_id++) {
+				if (VK_CONTROL == key_id || VK_SHIFT == key_id)
 					continue;
 
-				if (!GetAsyncKeyState(i))
+				if (!GetAsyncKeyState(key_id))
 					continue;
 
 				if (!on_callback)
 					continue;
 				
-				on_callback(i, is_ctrl_pressed(), is_shift_pressed());
+				on_callback(get_process_id(), key_id, is_ctrl_pressed(), is_shift_pressed());
 			}
 
 			Sleep(120);
 		}
 	}
 private:
-	std::function<void(uint32_t, bool, bool)> on_callback;
+	std::function<void(uint32_t, uint32_t, bool, bool)> on_callback;
 };
 #endif // !HOTKEY_MANAGER_H
